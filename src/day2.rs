1aff818a447db3fb0c717624d175f6d6ee8cb5c2
Idris Elmi <idriselmi0@gmail.com>
2024-12-02 18:09:03 +0100

    accept borrowing for solve functions, began part 2

--- src/day2.rs
+++ src/day2.rs
@@ -7,9 +7,11 @@ pub fn main() {
 
     let reports = parse_input(&input);
 
-    let solution = solve(reports);
+    let solution_1 = solve_1(&reports);
+    println!("{}", solution_1);
 
-    println!("{}", solution);
+    let solution_2 = solve_2(&reports);
+    println!("{}", solution_2);
 }
 
 fn parse_input(input: &str) -> Vec<Vec<i32>> {
@@ -22,14 +24,14 @@ fn parse_line(line: &str) -> Vec<i32> {
         .collect()
 }
 
-fn solve(reports: Vec<Vec<i32>>) -> i32 {
+fn solve_1(reports: &[Vec<i32>]) -> i32 {
     reports
         .iter()
-        .filter(|report| is_safe_report(report))
+        .filter(|report| is_safe_report_1(report))
         .count() as i32
 }
 
-fn is_safe_report(report: &[i32]) -> bool {
+fn is_safe_report_1(report: &[i32]) -> bool {
     let is_increasing = report.windows(2).all(|pair| pair[0] < pair[1]);
     let is_decreasing = report.windows(2).all(|pair| pair[0] > pair[1]);
 
@@ -39,4 +41,15 @@ fn is_safe_report(report: &[i32]) -> bool {
     });
 
     (is_increasing || is_decreasing) && is_valid_range
+}
+
+fn solve_2(reports: &[Vec<i32>]) -> i32 {
+    reports
+        .iter()
+        .filter(|report| is_safe_report_2(report))
+        .count() as i32
+}
+
+fn is_safe_report_2(report: &[i32]) -> bool {
+    todo!();
 }

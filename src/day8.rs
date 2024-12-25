b216f631344056e03d3b93fd0ebfb2cefe0ab4f6
Idris Elmi <idriselmi0@gmail.com>
2024-12-25 04:20:55 +0100

    aa

--- src/day8.rs
+++ src/day8.rs
@@ -1,32 +1,33 @@
 use itertools::Itertools;
 use std::{
     collections::{HashMap, HashSet},
-    fs, usize,
+    fs, isize, usize,
 };
 
 #[allow(dead_code)]
 pub fn main() {
     let input = fs::read_to_string("inputs/day8.txt").expect("Unable to open file");
-    let antennas = parse_antennas(&input);
     let grid = parse_grid(&input);
+    let antennas = parse_antennas(&grid);
 
-    let solution = solve(&grid, &antennas);
-    println!("{}", solution);
+    let solution_1 = solve_1(&grid, &antennas);
+    let solution_2 = solve_2(&grid, &antennas);
+    println!("{}", solution_1);
+    println!("{}", solution_2);
 }
 
 fn parse_grid(input: &str) -> Vec<Vec<char>> {
     input.lines().map(|line| line.chars().collect()).collect()
 }
 
-fn parse_antennas(input: &str) -> HashMap<char, Vec<(usize, usize)>> {
-    input
-        .lines()
+fn parse_antennas(grid: &[Vec<char>]) -> HashMap<char, Vec<(usize, usize)>> {
+    grid.iter()
         .enumerate()
         .flat_map(|(row, line)| {
-            line.chars()
+            line.iter()
                 .enumerate()
-                .filter(|(_, c)| *c != '.' && *c != '\n')
-                .map(move |(col, c)| (c, (row, col)))
+                .filter(|(_, &c)| c != '.')
+                .map(move |(col, &c)| (c, (row, col)))
         })
         .fold(HashMap::new(), |mut acc, (c, pos)| {
             acc.entry(c).or_insert(Vec::new()).push(pos);
@@ -34,15 +35,31 @@ fn parse_antennas(input: &str) -> HashMap<char, Vec<(usize, usize)>> {
         })
 }
 
-fn solve(grid: &[Vec<char>], antennas: &HashMap<char, Vec<(usize, usize)>>) -> usize {
-    let antinodes: HashSet<(usize, usize)> = antennas
-        .values()
-        .flat_map(|positions| positions.iter().permutations(2))
-        .filter_map(|pair| {
+fn solve_1(grid: &[Vec<char>], antennas: &HashMap<char, Vec<(usize, usize)>>) -> usize {
+    let mut antinodes = HashSet::new();
+    for positions in antennas.values() {
+        for pair in positions.iter().permutations(2) {
             let (from, to) = (pair[0], pair[1]);
-            find_antinode(grid, *from, *to)
-        })
-        .collect();
+            if let Some(antinode) = find_antinode(grid, *from, *to) {
+                antinodes.insert(antinode);
+            }
+        }
+    }
+    antinodes.len()
+}
+
+fn solve_2(grid: &[Vec<char>], antennas: &HashMap<char, Vec<(usize, usize)>>) -> usize {
+    let mut antinodes = HashSet::new();
+    for positions in antennas.values() {
+        for pair in positions.iter().permutations(2) {
+            let (from, to) = (pair[0], pair[1]);
+            if let Some(antinodes_) = find_antinodes(grid, *from, *to) {
+                for antinode in antinodes_ {
+                    antinodes.insert(antinode);
+                }
+            }
+        }
+    }
     antinodes.len()
 }
 
@@ -63,6 +80,34 @@ fn find_antinode(
         Some((antinode_row as usize, antinode_col as usize))
     } else {
         None
+    }
+}
+
+fn find_antinodes(
+    grid: &[Vec<char>],
+    from: (usize, usize),
+    to: (usize, usize),
+) -> Option<Vec<(usize, usize)>> {
+    let (from_row, from_col) = from;
+    let (to_row, to_col) = to;
+
+    let mut antinodes = Vec::new();
+    let mut current_row = from_row as isize;
+    let mut current_col = from_col as isize;
+
+    let row_step = from_row as isize - to_row as isize;
+    let col_step = from_col as isize - to_col as isize;
+
+    while in_bounds(current_row, current_col, grid) {
+        antinodes.push((current_row as usize, current_col as usize));
+        current_row -= row_step;
+        current_col -= col_step;
+    }
+
+    if antinodes.is_empty() {
+        None
+    } else {
+        Some(antinodes)
     }
 }
 
